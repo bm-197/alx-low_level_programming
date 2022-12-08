11ced680fa5bf1ba449e9f6d47d02e0ab75ed8c6
Bisrat maru Shibeshi <111707587+bm-197@users.noreply.github.com>
2022-12-08 23:36:19 +0300

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,9 +2,9 @@
 
 /**
  * main - Entry point
- *
- * Return : Always 0 (success)
+ * Return: Always 0 (success)
  */
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
