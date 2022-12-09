50654dc3d960d03230938cbda3b3c69b2cf493fe
Bisrat maru Shibeshi <111707587+bm-197@users.noreply.github.com>
2022-12-09 18:43:11 +0300

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,19 +9,14 @@
 int main(void)
 {
 	int num1, num2;
-	 for (num1 = 48; num1 <= 57; num1++)
-	 {
-		 for (num2 = 48; num2 <= 57; num2++)
-		 {
-			 putchar(num1);
-			 putchar(num2);
-			 if (num1 + num2)
-			 {
-				 putchar(44);
-				 putchar(32);
-			 }
-		 }
-	 }
-	 putchar(10);
-	 return (0);
+	for (num1 = 48; num1 <= 56; num1++)
+	{
+		for (num2 = 49; num2 <= 57; num2++)
+		{
+			putchar(num1);
+			putchar(num2);
+		}
+	}
+	putchar(10);
+	return (0);
 }
