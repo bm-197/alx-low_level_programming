caf062da6dd519e4d5584b7df6f8d3c393667ba7
bm-197 <bisratmaru111@gmail.com>
2022-12-09 07:36:03 -0800

    possible combination of 2 digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	 for (num1 = 48; num1 <= 57; num1++)
+	 {
+		 for (num2 = 48; num2 <= 57; num2++)
+		 {
+			 putchar(num1);
+			 purchar(num2);
+			 if (num1 + num2)
+			 {
+				 putchar(44);
+				 puutchar(32);
+			 }
+		 }
+	 }
+	 putchar(10);
+	 return (0);
+}
