08d4e4a97383d5d2bb2f2c2e2e9311a8a0581f11
bm-197 <bisratmaru111@gmail.com>
2022-12-09 06:02:33 -0800

    lower case w//out e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Entry poinnt
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int ch = 'a';
+
+	while (ch <= 'z')
+	{
+		if (ch == e && ch == q)
+			continue;
+		ch++;
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
