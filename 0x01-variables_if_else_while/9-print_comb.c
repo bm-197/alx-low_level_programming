e17d5cfa632c10c4ca3024293cb9ce6b24797dea
bm-197 <bisratmaru111@gmail.com>
2022-12-09 07:16:38 -0800

    possible combination of single digit

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main -Entry point
+ * Retutn: Always 0 (sucess)
+ */
+
+int main(void)
+{
+	int num = 48;
+
+	while (num < 58)
+	{
+		putchar(num);
+		if (num < 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		num++;
+	}
+	putchar('\n');
+	return (0);
+}
