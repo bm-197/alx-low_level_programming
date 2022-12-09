f22fbed12ebf28ea9cc86364156a3fefee7f365f
Bisrat maru Shibeshi <111707587+bm-197@users.noreply.github.com>
2022-12-09 17:41:06 +0300

    print numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdion.h>
+
+/**
+* main - Entry point
+* Return: Always 0 (success)
+*/
+
+int main(void)
+{
+        int num = 48;
+        
+        while (num <= 57)
+        {
+                putchar(num);
+                num++;
+        }
+        putchar('\n');
+        return (0);
+}
