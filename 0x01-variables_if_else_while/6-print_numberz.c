c4fe7b9cb52a3464c08e4553932fef39851e2640
bm-197 <bisratmaru111@gmail.com>
2022-12-09 06:49:04 -0800

    print numbers

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -8,13 +8,14 @@
 
 int main(void)
 {
-        int num = 48;
+	int num = 48;
 
-        while (num <= 57)
-        {
-                putchar(num);
-                num++;
-        }
-        putchar('\n');
-        return (0);
+	while (num <= 57)
+	{
+          	putchar(num);
+             	num++;
+      	}
+      	putchar('\n');
+      	return (0);
+
 }
