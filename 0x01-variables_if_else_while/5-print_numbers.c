64b4a5108440a76f3f565591fab4032bdbde352b
bm-197 <bisratmaru111@gmail.com>
2022-12-09 06:28:48 -0800

    nums form 0 to 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	for (num = 0, num <= 9, num++)
+	{
+		printf("%d\n", num);
+	}
+	return (0);
+}
