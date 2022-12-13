ddee375b80663b09469810384ff0947a56499a80
bm-197 <bisratmaru111@gmail.com>
2022-12-13 07:48:21 -0800

    time counter

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * jack_baure - print every minute of the day
+ */
+
+int jack_baue(void)
+{
+	int i, j, k, l;
+	for (i = 0; i <= 2; i++)
+	{
+		_putchar(i);
+		for (j = 0; j <= 3; j++)
+		{
+			_putchar(j);
+			_putchar(':');
+			_putchar('\n');
+		}
+	}
+}
