01ff9c81eec84fab716f9c690b3187d6279635e2
bm-197 <bisratmaru111@gmail.com>
2022-12-13 02:03:39 -0800

    print putchar

---
+++ 0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	printf("__putchar");
+	return 0;
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+low level c programing languuage
