c30bc802a5ad12e29169a643441cc25ef09a2c33
bm-197 <bisratmaru111@gmail.com>
2022-12-13 07:33:13 -0800

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints last digit
+ * @n: value to be analayzed
+ * Return: last digit
+ */
+
+int print_last_digit(int n)
+{
+	n = n % 10
+
+	if (n < 0)
+	{
+		n = -n
+	}
+	_putchar(n + '0')
+	return (n);
+}
+
