f75b37b383ed7a513a6879bc3a7a3c13181bb4f9
bm-197 <bisratmaru111@gmail.com>
2022-12-13 06:04:14 -0800

    alphaber

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a', letter <= 'z', letter++)
+	{
+		_putchar(letter);
+	}
+
+	_putchar("\n");
+
+}
