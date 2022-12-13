57f0e1c60c00b9794c50b873e972cd40378a5db7
Bisrat Maru Shibeshi <111707587+bm-197@users.noreply.github.com>
2022-12-13 20:49:28 +0300

    time counter

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -13,11 +13,11 @@ void jack_bauer(void)
 	{
 		for (min = 0; min < 60; min++)
 		{
-			_putchar((hr / 10));
-			_putchar((hr % 10) );
+			_putchar((hr / 10) + '0');
+			_putchar((hr % 10) + '0');
 			_putchar(':');
-			_putchar((min / 10));
-			_putchar((min % 10));
+			_putchar((min / 10) + '0');
+			_putchar((min % 10) + '0');
 			_putchar('\n');
 		}
 	}
