742ef749881d66c9b0b2ef8798e9e8cd950ba9e5
bm-197 <bisratmaru111@gmail.com>
2022-12-13 02:40:22 -0800

    print string

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,7 +5,7 @@
  * Return: Always 0 (success)
  */
 
-int main()
+int main(void)
 {
 	_putchar('_');
 	_putchar('p');
@@ -16,5 +16,5 @@ int main()
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n')
-	return 0;
+	return (0);
 }
