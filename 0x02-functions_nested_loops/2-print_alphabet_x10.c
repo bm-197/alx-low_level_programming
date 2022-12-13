ad054ce3b11bf06865b076dc0bbf9590b753c000
Bisrat Maru Shibeshi <111707587+bm-197@users.noreply.github.com>
2022-12-13 17:35:41 +0300

    alphabet 10x

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,20 +1,33 @@
 #include "main.h"
 
 /**
+ * print_alphabet - prints lower case alphabet
+ */
+
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		_putchar(letter);
+	}
+
+	_putchar('\n');
+
+}
+
+/**
  * print_alphabet_x10 - print lower case alphabet 10x
  */
 
 void print_alphabt_x10(void)
 {
-	char letter;
 	int i;
+	
 	for (i = 0; i <= 10; i++)
 	{
-		for (letter = 'a'; letter <= 'z'; letter++)
-		{
-			_putchar(letter);
-		}
-		_putchar('\n');
+		print_alphabet();
 	}	
 }
 
