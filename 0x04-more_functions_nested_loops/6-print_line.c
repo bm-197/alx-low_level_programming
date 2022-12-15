ed62a94ca39ce18f5927c24d196612eb2afdd78d
Bisrat Maru Shibeshi <111707587+bm-197@users.noreply.github.com>
2022-12-15 20:22:23 +0300

    draw line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -9,13 +9,17 @@ void print_line(int n)
 {
 	int i;
 
-	if (n <= 0)
-	{
-		_putchar('\n');
-	}
+	
 	for (i = 1; i <= n; i++)
 	{
-		_putchar('_');
+		if (n > 0)
+		{
+			_putchar('_');	
+		}
+		else
+		{
+			_putchar('\n');
+		}
 	}
 	_putchar('\n');
 }
