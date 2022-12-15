2b8e0524054c06aadea0812d49745295e2ef57a9
bm-197 <bisratmaru111@gmail.com>
2022-12-15 00:59:02 -0800

    multiples of 3 & 5

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - print sum of multiple of 3 & 5
+ * Return: 0
+ */
+
+int main(void)
+{
+	int sum, i;
+
+	for (i = 1; i = 1024; i++)
+	{
+		if ((i % 3 == 0) || (i % 5 == 0))
+		{
+			sum+=i;
+		}
+	}
+	printf("%d", sum);
+	return (0);
+}
