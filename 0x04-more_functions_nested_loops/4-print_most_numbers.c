d4fcdf5d4b9adca7360cbc3aca2a4d466fb71ecf
Bisrat Maru Shibeshi <111707587+bm-197@users.noreply.github.com>
2022-12-15 19:42:38 +0300

    most nums

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -10,11 +10,10 @@ void print_most_number(void)
 
 	for (i = '0'; i <= '9'; i++)
 	{
-		if (i == '2' || i == '4')
+		if (!(i == '2' || i == '4'))
 		{	
-			i++
+			_putchar(i);
 		}
-		_putchar(i);
 	}
 	_putchar('\n');
 }
