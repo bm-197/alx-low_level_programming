ec5c343c43d266db244cc76798abd83c46b0fa7b
bm-197 <bisratmaru111@gmail.com>
2022-12-15 01:45:46 -0800

    fibionacci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdion.h>
+
+/**
+ * main - fibonacci
+ * Return: 0
+ */
+
+int main(void)
+{
+	unsigned long num1 = 0, num2 = 1, num3, sum = 0;
+
+	while (num3 < 4000000)
+	{
+		num3 = num1 + num2;
+		num1 = num2;
+		num2 = num3;
+		if (num3 %2 == 0)
+		{
+			sum += z;
+		}
+	}
+	prinf("%lu\n", sum)
+	return (0);
+}
