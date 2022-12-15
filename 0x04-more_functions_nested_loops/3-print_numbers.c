35d44c05799e8477c6075e2be04a433395932b5f
bm-197 <bisratmaru111@gmail.com>
2022-12-15 08:31:13 -0800

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * pritn_numbers - print number 0 to 9
+ */
+
+void print_numbers(void)
+{
+	char i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
+
