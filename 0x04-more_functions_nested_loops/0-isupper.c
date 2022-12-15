099d8256807208bc56ebd35f11127442caf69c4e
Bisrat Maru Shibeshi <111707587+bm-197@users.noreply.github.com>
2022-12-15 19:26:44 +0300

    upper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -10,14 +10,10 @@ int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
 	{
-		_putchar(c);
-		_putchar(':');
 		return (1);
 	}
 	else
 	{
-		_putchar(c);
-		_putchar(':');
 		return (0);
 	}
 	_putchar('\n');
