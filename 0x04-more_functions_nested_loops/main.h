5aa84f91eb010035f8dc663bf48373c1bdf786b3
bm-197 <bisratmaru111@gmail.com>
2022-12-15 07:21:42 -0800

    header

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,5 +1,4 @@
-#ifndef MAIN_H
-#define MAIN_H
+#include <unistd.h>
 
 int _putchar(char);
 int _isupper(int c);
@@ -15,4 +14,4 @@ void print_triangle(int size;
 void print_number(int n);
 
 
-#endif
+
