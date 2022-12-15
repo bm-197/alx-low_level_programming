36a527813f8f63490161afb2bbae04d332e7cac0
bm-197 <bisratmaru111@gmail.com>
2022-12-15 09:03:44 -0800

    line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_line - draw a straight line
+ * @n - size of the line
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	for (i = 1; i <= n; i++)
+	{
+		if (n <= 0)
+		{
+			_putchar('\n');
+		}
+		else
+		{
+			_putchar('_');
+		}
+	}
+	_putchar('\n');
+}
