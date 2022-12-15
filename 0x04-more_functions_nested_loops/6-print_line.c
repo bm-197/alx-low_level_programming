ce6d585e836940ea4c5f3dae801801fdf978b619
Bisrat Maru Shibeshi <111707587+bm-197@users.noreply.github.com>
2022-12-15 20:19:02 +0300

    line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -8,17 +8,14 @@
 void print_line(int n)
 {
 	int i;
-
+	
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
 	for (i = 1; i <= n; i++)
 	{
-		if (n > 0)
-		{
-			_putchar('_');
-		}
-		else
-		{
-			_putchar('\n');
-		}
+		_putchar('_');
 	}
 	_putchar('\n');
 }
