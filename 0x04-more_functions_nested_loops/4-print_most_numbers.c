abe28d938a1bac402f1475356c33493bae71b219
bm-197 <bisratmaru111@gmail.com>
2022-12-15 08:38:08 -0800

    most nums

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - print number
+ */
+
+void print_most_number(void)
+{
+	char i;
+
+	for (i >= 0; i <= 9; i++)
+	{
+		if (i == 2 || i == 4)
+		{	
+			i++
+		}
+		_putchar(i);
+	}
+	_putchar('\n');i
+}
