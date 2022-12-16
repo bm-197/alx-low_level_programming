08fe6bec962ae3e29df567096df0eb3759fcb97f
Bisrat Maru Shibeshi <111707587+bm-197@users.noreply.github.com>
2022-12-16 13:27:08 +0300

    numbers 10x

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -12,10 +12,14 @@ void more_numbers(void)
 	{
 		for (j = 0; j <= 14; j++)
 		{
-			if (j > = 10)
+			if (j >= 10)
 			{
 				_putchar('1');
 				_putchar(j % 10 + '0');
+			}
+			else
+			{
+				_putchar(j);
 			}
 		}
 		_putchar('\n');	
