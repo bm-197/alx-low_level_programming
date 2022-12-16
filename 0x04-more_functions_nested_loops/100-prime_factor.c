b4582d2298dccdaefc81950d073dad8980278208
Bisrat Maru Shibeshi <111707587+bm-197@users.noreply.github.com>
2022-12-16 12:42:37 +0300

    prime factor

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - Entry Point
+ * main - prints prime factor of the given
  * Return: Always 0 (success)
  */
 
@@ -9,11 +9,11 @@ int main(void)
 {
 	unsigned long i, n = 612852475143;
 
-	for (i = 3; i <= 782849; i = i + 2)
+	for (i = 3; i < 782849; i = i + 2)
 	{
 		while ((n % i == 0) && (n != i))
 		{
-			n =n / i;
+			n = n / i;
 		}
 	}
 	printf("%lu\n", n);
