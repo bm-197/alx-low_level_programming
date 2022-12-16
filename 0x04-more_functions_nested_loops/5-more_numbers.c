5477747bf816dfc6c60a8fcf0a2877bffe151ea2
Bisrat Maru Shibeshi <111707587+bm-197@users.noreply.github.com>
2022-12-16 13:30:54 +0300

    numbers 10x

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -2,13 +2,14 @@
 
 /**
  * more_numbers - prints from 0 to 9 except 2 and 4
+ * Return: no return
  */
 
 void more_numbers(void)
 {
 	int i, j;
 
-	for (i = 0; i <= 10; i++)
+	for (i = 1; i <= 10; i++)
 	{
 		for (j = 0; j <= 14; j++)
 		{
