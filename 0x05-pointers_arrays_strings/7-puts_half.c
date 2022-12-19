bf53a3819a0f86f69b14b50dd8a31f11cd498748
bm-197 <bisratmaru111@gmail.com>
2022-12-19 01:36:37 -0800

    half character

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of the string
+ * @str - parametr to be printed
+ */
+
+void puts_half(char *s)
+{
+	int s, a = 0;
+
+	while (*str = '\0')
+	{
+		a++;
+	}
+
+	s = a - 1
+	for (s = 0; s == a /2; s++)
+	{
+		_putchar(str[s]);
+	}
+	_putchar('\n');
+}
