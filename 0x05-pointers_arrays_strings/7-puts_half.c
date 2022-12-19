5ef4bffa454cd44fbd0b1e81780eb6e7e3bb84bd
Bisrat Maru Shibeshi <111707587+bm-197@users.noreply.github.com>
2022-12-19 12:50:42 +0300

    puts half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -7,13 +7,14 @@
 
 void puts_half(char *str)
 {
-	int a = 1, s;
+	int a = 0, s;
 
 	while (str[a] != '\0')
 	{
 		a++;
 	}
-	for (s = a / 2; str[s] != '\0'; s++)
+	s = a / 2
+	for (s = s - 1; str[s] != '\0'; s++)
 	{
 		_putchar(str[s]);
 	}
