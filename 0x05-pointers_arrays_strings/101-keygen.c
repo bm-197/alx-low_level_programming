aca519a44473245fd4b2db5e5c4dcdd72e21dfac
bm-197 <bisratmaru111@gmail.com>
2022-12-19 08:23:54 -0800

    password

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,44 @@
+#include "main.h"
+#include "stdlib.h"
+#include "time.h"
+#include <stdio.h>
+
+/**
+ * main - generate password
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char password[84];
+	int index = 0, sum = 0, diff_half1, diff_half2;
+
+	srand(time(0));
+
+	while (sum < 2772)
+	{
+		password[index] = 33 + rand() % 94;
+		sum += password[index++];
+	}
+	password[index] = '\0';
+
+	if (sum != 2772)
+	{
+		diff_half1 = (sum - 2772) / 2;
+		diff_half2 = (sum - 2771) / 2;
+		if ((sum - 2772) % 2 != 0)
+		{
+			diff_half1++;
+		}
+		for (index = 0; password[index]; index++)
+		{
+			if (password[index] >= (33 + diff_half2))
+			{
+				password[index] -= diff_half2;
+				break;
+			}
+		}
+	}
+	printf("%s", password);
+	return (0);
+}
