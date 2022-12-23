e86205b184e4c4786f91d0aa1bdc2ae5661c75af
Bisrat Maru Shibeshi <111707587+bm-197@users.noreply.github.com>
2022-12-23 17:32:27 +0300

    compare

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,12 +9,9 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	while (*s1 != '\0')
+	while (((*s1 != '\0') && (*s2 != '\0')) && (*s1 == *s2))
 	{
 		s1++;
-	}
-	while (*s2 != '\0')
-	{
 		s2++;
 	}
 	if ( *s1 == *s2)
@@ -25,3 +22,5 @@ int _strcmp(char *s1, char *s2)
 	{
 		return (*s1 - *s2);
 	}
+}
+
