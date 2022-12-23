c94549498a3b62af76eec7e8bda2e5511506ae51
bm-197 <bisratmaru111@gmail.com>
2022-12-23 07:06:28 -0800

    lower to upper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes all lower case to uppper
+ * @s: parameter
+ * Return: char
+ */
+
+char *string_toupper(char *s)
+{
+	int i;
+	
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+		{
+			s[i] -= 'a' - 'A';
+		}
+		return(s);
+	}
+}
