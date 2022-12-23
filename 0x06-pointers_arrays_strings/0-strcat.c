c82b57f8e4241b72411899c4ed537c830dcdef19
Bisrat Maru Shibeshi <111707587+bm-197@users.noreply.github.com>
2022-12-23 16:48:54 +0300

    concatenate

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,6 +9,8 @@
 
 char *_strcat(char *dest, char *src)
 {
+	char *s = dest;
+
 	while (*dest != '\0')
 	{
 		dest++;
@@ -20,5 +22,5 @@ char *_strcat(char *dest, char *src)
 		src++;
 	}
 	*dest = '\0';
-	return (dest);
+	return (s);
 }
