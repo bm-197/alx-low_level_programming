1ff344b302b0a8992acd373fadc5608317bc982c
bm-197 <bisratmaru111@gmail.com>
2022-12-25 07:38:08 -0800

    buffer

---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_buffer - prints a buffer
+ * @b: parameter
+ * @size: parameter
+ * Return: char
+ */
+
+void print_buffer(char *b, int size)
+{
+	int i
+}
