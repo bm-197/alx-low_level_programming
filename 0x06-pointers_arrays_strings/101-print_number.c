d3234db2fd07d0000736d1226507b9cf2ecb9578
bm-197 <bisratmaru111@gmail.com>
2022-12-25 06:08:50 -0800

    print integer

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints intiger
+ * @n: parameter
+ * Return: nothing
+ */
+
+void print_number(int n)
+{
+	   unsigned int i;
+
+	   if (n < 0)
+	   {
+		   _putchar('-');
+		   n *= -1;
+	   }
+
+	   i = n;
+	   
+	   if (i / 10)
+	   {
+		   print_number(i / 10);
+	   }
+	   _putchar(i % 10 + '0');
+}
