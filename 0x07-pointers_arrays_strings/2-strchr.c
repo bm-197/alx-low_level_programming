d5ec97f032c4c34aec0ec334d8ba39420b8f416b
bm-197 <bisratmaru111@gmail.com>
2023-01-04 08:25:51 -0800

    strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,20 +9,20 @@
 
 char *_strchr(char *s, char c)
 {
-        while (*s)
-        {
-                  if (*s != c)
-                  {
-                          s++;
-                  }
-                  else
-		  {         
-			  return (s);
-		  }
-        }
-        if (c == '\0')
-        {
-                  return (s);
-        }
-        return (0);
+	while (*s)
+	{
+		if (*s != c)
+		{
+			s++;
+		}
+		else
+		{
+			return (s);
+		}
+	}
+	if (c == '\0')
+	{
+		return (s);
+	}
+	return (0);
 }
