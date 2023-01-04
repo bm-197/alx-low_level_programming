345e63206231b953e33c6ffc005ca730231f6a6c
Bisrat Maru Shibeshi <111707587+bm-197@users.noreply.github.com>
2023-01-04 19:44:47 +0300

    strspn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -6,9 +6,9 @@
  * @accept: parameter
  * Return: s
  */
-unsigned int *_strspn(char *s, char *accept)
+unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int *i = 0;
+	unsigned int i = 0;
         int x, y;
 
 	for (x = 0; s[x] != '\0'; x++)
