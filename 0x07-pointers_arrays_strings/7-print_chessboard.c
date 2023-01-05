354f8100bcb09b24afb4241c4b93af770cccf997
bm-197 <bisratmaru111@gmail.com>
2023-01-05 08:05:48 -0800

    chess board

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -7,7 +7,7 @@
 
 void print_chessboard(char (*a)[8])
 {
-	int i = j = 0;
+	int i = 0, j = 0;
 
 	for (; i < 8; i++)
 	{
@@ -17,4 +17,4 @@ void print_chessboard(char (*a)[8])
 		}
 		_putchar('\n');
 	}
-}
+
