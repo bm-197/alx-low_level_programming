9ca643fb0a69ec302b5864e8791f3e85259ba2e5
bm-197 <bisratmaru111@gmail.com>
2023-01-06 05:00:18 -0800

    prime

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * checker - prime number
+ * @x: int
+ * @y: int
+ */
+
+int checker(int x, int y)
+{
+	if (x % y == 0)
+	{
+		return (0);
+	}
+	else if (x == y)
+	{
+		return (1);
+	}
+	return (checker(x, y + 1));
+	
+}
+/**
+ * is_prime_number - prime number checker
+ * @n: int
+ * Return: int
+ */
+
+int is_prime_number(int n)
+{
+	if (n == 2)
+	{
+		return (1);
+	}
+	if (n < 2 || n == 0)
+	{
+		return (0);
+	}
+	return (checker(n, 2));
+}
