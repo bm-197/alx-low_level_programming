04ea0e79b06c540ec24aad52ec70d3c6d37a176a
bm-197 <bisratmaru111@gmail.com>
2023-01-06 04:36:00 -0800

    square root

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * checker - square root
+ * @x: int
+ * @y: int
+ * Return: int
+ */
+
+int checker(int x, int y)
+{
+	if (x * x == y)
+	{
+		return (x);
+	}
+	if (x * x > y)
+	{
+		return (-1);
+	}
+	checker(x + 1, y);
+}
+
+/**
+ * _sqrt_recursion -  square root
+ * @n: int
+ * Return: int
+ */
+
+
+int _sqrt_recursion(int n)
+{
+	if (n == 0)
+	{
+		return (0);
+	}
+	return (checker(1, n));
+}
