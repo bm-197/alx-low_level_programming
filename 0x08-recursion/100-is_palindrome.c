7a665128d940a7af4dc13ddb68eed95a7de20d2f
bm-197 <bisratmaru111@gmail.com>
2023-01-06 05:40:39 -0800

    palindrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,15 +1,20 @@
 #include "main.h"
+
 /**
  * _strlen_recursion - Prints the length of a string.
  * @s: the string to be printed
  * Return: the length of string
  */
+
 int _strlen_recursion(char *s)
 {
 	if (s[0] != '\0')
+	{
 		return (1 + _strlen_recursion(s + 1));
+	}
 	return (0);
 }
+
 /**
  * pal_checker - check if s is palindrome.
  * @s: string base address.
@@ -20,12 +25,19 @@ int _strlen_recursion(char *s)
 int pal_checker(char *s, int i, int j)
 {
 	if (s[i] == s[j])
+	{
 		if (i > j / 2)
+		{
 			return (1);
+		}
 		else
+		{
 			return (pal_checker(s, i + 1, j - 1));
-	else
-		return (0);
+		}
+		else
+		{
+			return (0);
+		}
 }
 /**
  * is_palindrome - check if s is palindrome
