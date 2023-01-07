df71b6da84c17cbbcbeff6e3c88bc51edf8e3491
bm-197 <bisratmaru111@gmail.com>
2023-01-06 21:03:06 -0800

    what-is-my-name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry
+ * @argc: number of the argument
+ * @argv: array of the argument
+ * Return: Always 0 (success)
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+
+	printf("%s\n", argv[0]);
+	return (0);
+}
