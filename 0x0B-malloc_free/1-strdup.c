8db48459f6fc16e64f9f4f30a1a1ddbcb695d124
bm-197 <bisratmaru111@gmail.com>
2023-01-11 05:18:50 -0800

    strdup

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * _strdup - function
+ * @str: parameter
+ * Return: NULL
+ */
+
+char *_strdup(char *str)
+{
+	char *copy;
+	int i, len = 0;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	while (str[len] = '\0')
+	{
+		len++;
+	}
+	copy = (char *)malloc((sizeof(char) * len) + 1);
+	if (copy == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < len; i++)
+	{
+		copy[i] = str[i];
+	}
+	copy[len] = '\0';
+	return (copy);
+}
