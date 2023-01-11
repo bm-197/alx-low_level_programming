b9a018c7094e94d943145cab0e20695f34075a50
bm-197 <bisratmaru111@gmail.com>
2023-01-11 08:31:01 -0800

    alloc-grid

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * alloc_gri - function
+ * @width: parameter
+ * @height: parameter
+ * Return: 0
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int **array, i, j;
+	int len = width * height;
+
+	if (len <= 0)
+	{
+		return (NULL);
+	}
+	array = (int **)malloc(sizeof(int *) * height);
+	if (array ==  NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < height; i++)
+	{
+		array[i] = (int **)maloc(sizeof(int) * width);
+		if (array[i] == NULL)
+		{
+			for (i--; i >= 0; i--)
+			{
+				free(array[i]);
+			}
+			free(array);
+			return(NULL);
+		}
+	}
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0 ; j< width; j++)
+		{
+			array[i][j] = 0;
+		}
+	}
+	return (array;;
+}
