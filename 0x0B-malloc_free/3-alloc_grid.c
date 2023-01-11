25222452969604c092dbf4b21215e012787bf8b1
Bisrat Maru Shibeshi <111707587+bm-197@users.noreply.github.com>
2023-01-11 19:33:14 +0300

    alloc_grid

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -23,7 +23,7 @@ int **alloc_grid(int width, int height)
 	}
 	for (i = 0; i < height; i++)
 	{
-		array[i] = (int **)maloc(sizeof(int) * width);
+		array[i] = (int **)malloc(sizeof(int) * width);
 		if (array[i] == NULL)
 		{
 			for (i--; i >= 0; i--)
@@ -41,5 +41,5 @@ int **alloc_grid(int width, int height)
 			array[i][j] = 0;
 		}
 	}
-	return (array;;
+	return (array;
 }
