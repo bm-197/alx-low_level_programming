e8a6d69c99d52bd3d3a4fc552aeae34bf08d183d
Bisrat Maru Shibeshi <111707587+bm-197@users.noreply.github.com>
2023-01-11 19:21:24 +0300

    str_concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -30,24 +30,23 @@ char *str_concat(char *s1, char *s2)
 			len2++;
 		}
 	}
-
 	len = len1 + len2;
 	result = (char *)malloc(sizeof(char) * (len + 1));
-
-	for (i = 0; i < len1; i++)
+	if (result == NULL)
 	{
-		result[i] = s1[i];
+		return (NULL);
+	}
+	if (result != NULL)
+	{
+		for (i = 0; i < len1; i++)
+		{
+			result[i] = s1[i];
+		}
 	}
 	for (j = 0; j < len2; j++)
 	{
 		result[i] = s2[j];
 	}
-	if (result == NULL)
-	{
-		i = 0;
-		return (NULL);
-	}
 	result[len] = '\0';
-
 	return (result);
 }
