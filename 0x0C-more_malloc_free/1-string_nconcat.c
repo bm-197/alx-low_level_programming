e73f39d6e55a425a8f1a0f1401cf9327894385aa
Bisrat Maru Shibeshi <111707587+bm-197@users.noreply.github.com>
2023-01-12 12:48:18 +0300

    Update 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -14,44 +14,28 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *result;
 
 	if (s1 == NULL)
-	{
 		i = 0;
-	}
 	else
 	{
 		for (i = 0; s1[i]; )
-		{
 			++i;
-		}
 	}
 	if (s2 == NULL)
-	{
 		j = 0;
-	}
 	else
 	{
 		for (j = 0; s2[j]; )
-		{
 			++j;
-		}
 	}
 	if (j > n)
-	{
 		j = n;
-	}
 	result = malloc(sizeof(char) * i + j + 1);
 	if (result == NULL)
-	{
 		return (NULL);
-	}
 	for (len = 0; len < i; len++)
-	{
 		result[len] = s1[len];
-	}
 	for (len = 0; len < j; j++)
-	{
 		result[len + i] = s2[len];
-	}
 	result[i + j] = '\0';
 	return (result);
 }
