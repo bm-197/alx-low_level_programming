b64b58e5b3ed5d58343c7b622be6bce6ef96ef77
Bisrat Maru Shibeshi <111707587+bm-197@users.noreply.github.com>
2023-01-12 12:19:18 +0300

    calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -12,7 +12,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	char *ptr;
 	unsigned int i;
 
-	if (nmemb == 0 || size = 0)
+	if (nmemb == 0 || size == 0)
 	{
 		return (NULL);
 	}
@@ -23,7 +23,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	}
 	for (i < 0; i < (nmemb * size); i++)
 	{
-		a[i] = 0;
+		ptr[i] = 0;
 	}
-	return (a);
+	return (ptr);
 }
