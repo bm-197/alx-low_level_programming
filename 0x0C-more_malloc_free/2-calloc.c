1667d82324787d140fb1cf419785b3429de9650b
Bisrat Maru Shibeshi <111707587+bm-197@users.noreply.github.com>
2023-01-12 13:12:07 +0300

    calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,8 +1,7 @@
 #include "main.h"
 
 /**
- * _calloc -  allocates memory for an array of nmemb elements of size bytes each 
- * and returns a pointer to the allocated memory.
+ * _calloc -  function.
  * @nmemb: int
  * @size: int
  * Return: pointer or null
