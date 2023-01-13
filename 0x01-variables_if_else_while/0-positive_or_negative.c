a0c19793d974db32ac13f7eb7971639b94948109
Bisrat Maru Shibeshi <111707587+bm-197@users.noreply.github.com>
2023-01-13 10:03:20 +0300

    positive or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,7 +4,7 @@
 
 /**
  * main - Entry point
- * Return: Always 0 (success)\
+ * Return: Always 0 (success)
  */
 
 int main(void)
@@ -13,18 +13,13 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+
 	if (n > 0)
-	{
 		printf("%d is positive\n", n);
-	}
 	else if (n == 0)
-	{
 		printf("%d is zero\n", n);
-	}
 	else
-	{
 		printf("%d is negative\n", n);
-	}
 	return (0);
 }
 
