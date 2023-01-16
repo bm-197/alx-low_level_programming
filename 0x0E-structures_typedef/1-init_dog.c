9a9a58d0a36cd7a14189e16adb8580053119486b
bm-197 <bisratmaru111@gmail.com>
2023-01-16 01:20:40 -0800

    intit dog

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,1 +1,24 @@
+#include "dog.h"
+/**
+ * init_dog - intializes a structure of type dog
+ *
+ * @d: pointer to structure
+ *
+ * @name: pointer to name of the dog
+ *
+ * @age: to age of the dog
+ *
+ * @owner: to owner of the dog
+ *
+ * Return: void
+ */
 
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d)
+	{
+		dog.name = name;
+		dog.age = age;
+		dog owner = owner;
+	}
+}
