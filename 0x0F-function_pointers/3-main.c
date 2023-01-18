ffb31b70ee418407dd8df1873459ca94086c89ce
Bisrat Maru Shibeshi <111707587+bm-197@users.noreply.github.com>
2023-01-18 23:17:51 +0300

    main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -9,18 +9,20 @@
  * Return: always 0
  */
  
-int main(int __attribute__((__unused__))argc, char *argv[])
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
 	int num1, num2;
 	char *op;
-	num1 = atoi(argv[1]);
-	op = argv[2];
-	num2 = atoi(argv[3]);
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
+	
+	num1 = atoi(argv[1]);
+	op = argv[2];
+	num2 = atoi(argv[3]);
+	
 	if (get_op_func(op) == NULL || op[1] !='\0')
 	{
 		printf("Error\n");
@@ -31,6 +33,6 @@ int main(int __attribute__((__unused__))argc, char *argv[])
 		printf("Error\n");
 		exit(100);
 	}
-	get_op_func(op)(num1, num2);
+	printf("%d\n", get_op_func(op)(num1, num2));
 	return (0);
 }
