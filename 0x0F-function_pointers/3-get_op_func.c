603999a6d407101e0bada8dc8cdbb0e1d36238e5
Bisrat Maru Shibeshi <111707587+bm-197@users.noreply.github.com>
2023-01-18 23:23:28 +0300

    Update 3-get_op_func.c

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -9,16 +9,16 @@
 
 int (*get_op_func(char *s))(int, int)
 {
-        opt_t oprs[] = {
-                {"+", op_add},
-                {"-", op_sub},
-                {"*", op_mul},
-                {"/", op_div},
-                {"%", op_mod},
-                {NULL, NULL}
-        };
-        int i = 0;
-        while(oprs[i].opr != NULL && *(oprs[i].opr) != *s)
-                i++;
-        return (oprs[i].opfun);
+	opt_t oprs[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i = 0;
+	while(oprs[i].opr != NULL && *(oprs[i].opr) != *s)
+		i++;
+	return (oprs[i].opfun);
 }
