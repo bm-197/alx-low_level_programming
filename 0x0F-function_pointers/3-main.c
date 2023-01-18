72d0012c0b23abc1191f77d6098722c4c551e44f
Bisrat Maru Shibeshi <111707587+bm-197@users.noreply.github.com>
2023-01-18 23:02:54 +0300

    main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -26,7 +26,7 @@ int main(int __attribute__((__unused__))argc, char *argv[])
 		printf("Error\n");
 		exit(99);
 	}
-	if ((*op == '/' && num2 == 0) || (*op == % && num2 == 0))
+	if ((*op == '/' && num2 == 0) || (*op == '%' && num2 == 0))
 	{
 		printf("Error\n");
 		exit(100);
