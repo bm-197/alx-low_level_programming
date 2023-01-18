e400863a63c40511c235c5029ad9de776e98fe3e
bm-197 <bisratmaru111@gmail.com>
2023-01-18 05:15:53 -0800

    int_index

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -12,11 +12,11 @@ int int_index(int *array, int size, int (*cmp)(int))
 	int i;
 
 	if (array == NULL || cmp == NULL)
-		return;
+		return (-1);
 	for (i = 0; i < size; i++)
 	{
 		if (cmp(array[i]) != 0)
-			return (index);
+			return (i);
 	}
 	if (size <= 0)
 		return (-1);
