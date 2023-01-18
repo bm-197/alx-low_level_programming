bfaa05544ebf190b795d973f7e72ad6113576d9a
Bisrat Maru Shibeshi <111707587+bm-197@users.noreply.github.com>
2023-01-18 22:30:19 +0300

    get_op_function

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -4,4 +4,20 @@
 /**
  * get_op_func - function selector
  * @s: oprator
- * Return: oprator functionhfhf
+ * Return: oprator function
+ */
+
+int (*get_op_func(char *s))(int, int)
+{
+        opt_t oprs[] = {
+                {"+", op_add};
+                {"-", op_sub};
+                {"*", op_mul};
+                {"/", op_div};
+                {"%", op_mod};
+                {NULL, NULL};
+        };
+        while(ops[i].opr != NULL && *(ops[i].opr) != *s)
+                i++;
+        return (ops[i].opfun);
+}
