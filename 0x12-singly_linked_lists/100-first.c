3967a5fc5702830a751f5b953ef2408cafbc25ea
bm-197 <bisratmaru111@gmail.com>
2023-01-26 08:46:49 -0800

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * _main - print string before
+ * the main func excutes
+ * Return: Always 0 (success)
+ */
+
+#define _main main
+
+int _main(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	return (0);
+}
