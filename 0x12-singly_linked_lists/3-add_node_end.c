c694efa4bff68828ce5a6cace9fc367248a3dae6
Bisrat Maru Shibeshi <111707587+bm-197@users.noreply.github.com>
2023-01-26 19:34:43 +0300

    3-add_node_end.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -31,7 +31,7 @@ list_t *add_node_end(list_t **head, const char *str)
 		len++;
 
 	newNode->str = dup;
-	newNode->len = len
+	newNode->len = len;
 	newNode->next = NULL;
 	if (*head == NULL)
 		*head = newNode;
