68846eccd3ac89a2cac1a93ea0e1994d5db68979
bm-197 <bisratmaru111@gmail.com>
2023-01-30 11:32:58 -0800

    free list

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_listint - frees a listint_t list
+ * @head: pointer to structure
+ *
+ * Return: void
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *temp;
+
+	while (head)
+	{
+		temp = head->next;
+		free(head->n);
+		free(head);
+		head = temp;
+	}
+}
