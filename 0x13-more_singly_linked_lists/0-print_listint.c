c7c71a61b20337942437df32cf5a601e8936a7e1
bm-197 <bisratmaru111@gmail.com>
2023-01-30 08:17:18 -0800

    header

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * print_listint -  prints all the elements of a listint_t list
+ * @h: pointer to the structure
+ * Return: size_t
+ */
+
+
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -17,5 +17,17 @@ listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
-
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * 
+ */
+typedef struct listint_s
+{
+    int n;
+    struct listint_s *next;
+} listint_t;
 #endif
