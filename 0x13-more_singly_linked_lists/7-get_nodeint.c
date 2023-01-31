b3b2489ae879c0e5fc9311c3daa75d5e65c66513
Bisrat Maru Shibeshi <111707587+bm-197@users.noreply.github.com>
2023-01-31 21:53:35 +0300

    8-sum_listint.c

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -10,17 +10,15 @@
 
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	listint_t *temp;
 	unsigned int i = 0;
 
 	if (head == NULL)
 		return (NULL);
 
-	temp = head;
 	while (i < index)
 	{
 		i++;
-		temp = temp->next;
+		head = head->next;
 	}
-	return (temp);
+	return (head);
 }
