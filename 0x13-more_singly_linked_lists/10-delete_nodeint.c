68541598658abc212190dc310c9d02551aa8db83
bm-197 <bisratmaru111@gmail.com>
2023-01-31 11:43:55 -0800

    10-delete_nodeint.c

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -15,8 +15,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 
 	if (*head == NULL)
 		return (-1);
-
-	if (indext = 0)
+	if (index = 0)
 	{
 		temp = *head;
 		*head = (*head)->next;
