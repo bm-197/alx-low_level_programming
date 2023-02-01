c180c969ef9ea2dbdec1bd37d297a6549b327d3a
Bisrat Maru Shibeshi <111707587+bm-197@users.noreply.github.com>
2023-02-01 11:40:21 +0300

    100-reverse_listint.c

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -9,7 +9,7 @@
 
 listint_t *reverse_listint(listint_t **head)
 {
-	listint_t *preNode, *curNode, *nextNode;
+	listint_t *preNode, *nextNode;
 
 	if (head == NULL || *head == NULL)
 		return (NULL);
