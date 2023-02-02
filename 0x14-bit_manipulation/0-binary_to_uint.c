d794feca60a4ee088b9e5d03de6365351e31a7a7
bm-197 <bisratmaru111@gmail.com>
2023-02-02 07:30:35 -0800

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -5,7 +5,7 @@
  * @b: pointing to a string of 0 and 1 chars
  *
  * Return: the converted number,
- *	-or 0 if 
+ *	-or 0 if
  *	-there is one or more chars in the string b that is not 0 or 1
  *	-b is NULL
  */
@@ -25,7 +25,7 @@ unsigned int binary_to_uint(const char *b)
 	{
 		if (b[i] != '0' && b[i] != '1')
 			return (0);
-	
+
 		uint += (b[i] - '0') * pow;
 		pow = pow * 2;
 	}
