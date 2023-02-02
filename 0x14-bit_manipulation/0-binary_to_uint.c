85a7c346b7584679cf412f20bebb49b39f5d1994
Bisrat Maru Shibeshi <111707587+bm-197@users.noreply.github.com>
2023-02-02 18:16:59 +0300

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -19,15 +19,14 @@ unsigned int binary_to_uint(const char *b)
 		return (0);
 
 	for (i = 0; b[i];)
-	{
 		i++;
-	}
 
 	for (i -= 1; i >= 0; i--)
 	{
-		if (b[i] != '0' || b[i] != '1')
+		if (b[i] != '0' && b[i] != '1')
 			return (0);
-		uint += (b[i] - 48) * pow;
+	
+		uint += (b[i] - '0') * pow;
 		pow = pow * 2;
 	}
 	return (uint);
