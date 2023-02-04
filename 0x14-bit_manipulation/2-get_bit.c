d8be50498ab4a981bdba6c60cfdc6bd2874b8f7d
bm-197 <bisratmaru111@gmail.com>
2023-02-04 00:08:04 -0800

    2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -14,7 +14,7 @@ int get_bit(unsigned long int n, unsigned int index)
 	if (index >= (sizeof(unsigned long int) * 8))
 		return (-1);
 
-	num_val = (num >> index) & 1;
+	num_val = (n >> index) & 1;
 
 	return (num_val);
 }
