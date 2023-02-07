fddb8837113923275462b31b180c772da1f1d0ed
Bisrat Maru Shibeshi <111707587+bm-197@users.noreply.github.com>
2023-02-07 11:04:17 +0300

    Update 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,5 +1,12 @@
 #include "main.h"
 
+/**
+ * main - copies the content of a file to another file
+ * @argc: no of arguments
+ * @argv: the arguments
+ *
+ * Return: Always 0(success)
+ */
 int main(int argc, char *argv[])
 {
 	int fd_r, fd_w, r, a, b;
@@ -26,7 +33,7 @@ int main(int argc, char *argv[])
 			exit(99);
 		}
 	}
-	if (r < 0)
+	if(r < 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from %s\n", argv[1]);
 		exit(98);
@@ -41,5 +48,5 @@ int main(int argc, char *argv[])
 			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_w);
 		exit(100);
 	}
-	return(0);
+	return (0);
 }
