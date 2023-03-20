f054db994c5ba5804c7ab9c7465d4f76a34b2d89
Bisrat Maru Shibeshi <111707587+bm-197@users.noreply.github.com>
2023-03-20 23:32:32 +0300

    Update 2-add_dnodeint.c

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -19,7 +19,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	new->n = n;
 	new->prev = NULL:
 	new->next = *head;
-	if (*head == NULL)
+	if (*head != NULL)
 	{
 		(*head)->prev = new;
 	}
