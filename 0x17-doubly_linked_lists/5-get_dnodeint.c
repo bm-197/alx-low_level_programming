38ee555d557da077bf2eab868ed4fd1d84695478
Bisrat Maru Shibeshi <111707587+bm-197@users.noreply.github.com>
2023-03-21 23:17:55 +0300

    Update 5-get_dnodeint.c

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -12,11 +12,12 @@
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	unsigned int i = 0;
+
 	while (i < index)
 	{
 		if (head == NULL)
 		{
-			return NULL;
+			return (NULL);
 		}
 		head = head->next;
 		i++;
