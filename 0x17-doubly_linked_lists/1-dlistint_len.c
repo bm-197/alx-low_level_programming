749493a520463e8874d0044ff5b8047e8ff45066
bm-197 <bisratmaru111@gmail.com>
2023-06-19 01:49:03 -0700

    task 1

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * dlistint_len - returns the number of elements 
+ * dlistint_len - returns the number of elements
  * in a linked dlistint_t list.
  * @h: head of list
  *
