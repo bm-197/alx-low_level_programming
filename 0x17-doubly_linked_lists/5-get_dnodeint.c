f4c7278d68441559bb35a2e3212c98756e59bcaa
bm-197 <bisratmaru111@gmail.com>
2023-06-20 01:35:29 -0700

    task 5

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - the nth node of a dlistint_t linked list.
+ * @head: head of list
+ * @index: index of the node, starting from 0
+ *
+ * Retrun: nth node, if the node does not exist, return NULL
+ */
+
+
