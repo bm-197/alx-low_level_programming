a6fa70acf100bb81ba74926ab26269de8a867762
Bisrat Maru Shibeshi <111707587+bm-197@users.noreply.github.com>
2023-06-20 13:05:08 +0300

    task 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -11,20 +11,30 @@
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_int *new;
+	dlistint_t *tmp = *h, *new;
+
+	if (idx == 0)
+		return (add_dnodeint(h, n));
+
+	for (; idx != 1; idx--)
+	{
+		tmp = tmp->next;
+		if (tmp == NULL)
+			return (NULL);
+	}
+
+	if (tmp->next == NULL)
+		return (add_dnodeint_end(h, n));
+
 
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
-	while (idx != 0)
-		{
-			if (*h == NULL)
-				return (NULL);
-			*h = (*h)->next;
-		}
-	new->next = (*h)->next;
-	((*h)->next)->prev = new;
-	(*h)->next = new;
-	new->prev = *h;
+	new->n = n;
+	new->prev = tmp;
+	new->next = tmp->next;
+	tmp->next->prev = new;
+	tmp->next = new;
+	
 	return (new);
 }
