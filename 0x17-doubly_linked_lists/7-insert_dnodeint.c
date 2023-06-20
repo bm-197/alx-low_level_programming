0f27bf46a3f9df74ae77bb2d79f1965b4e0a289c
Bisrat Maru Shibeshi <111707587+bm-197@users.noreply.github.com>
2023-06-20 12:57:05 +0300

    task 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -9,7 +9,7 @@
  * Return: the address of the new node, or NULL if it failed
  */
 
-dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_int *new;
 
