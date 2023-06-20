b2b78ced910478e9b6e90e04b0d4e0a87d616ef6
Bisrat Maru Shibeshi <111707587+bm-197@users.noreply.github.com>
2023-06-20 11:48:22 +0300

    task 5

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -7,5 +7,23 @@
  *
  * Retrun: nth node, if the node does not exist, return NULL
  */
-
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	int ind = 0;
+	while (head)
+	{
+		head = head->next;
+		ind++;
+	}
+	
+	if (index > ind)
+		return (NULL);
+	ind = 0;
+	while (ind <= index)
+	{
+		head = head->next;
+		ind++;
+	}
+	return (head)
+}
 
