c709b68dc465c39e1df14d2882416881a2a869b2
Bisrat Maru Shibeshi <bisratmaru111@gmail.com>
2023-10-26 09:29:57 +0300

    task 5

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Technical interview preparation"""
+"""Define island perimeter measuring fun"""
 
 def island_perimeter(grid):
     """Calculate the perimeter of the island
@@ -10,5 +10,22 @@ def island_perimeter(grid):
     Returns:
         the perimeter
     """
+    edges = 0
+    size = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == 1:
+                size += 1
+                if j > 0 and grid[i][j - 1] == 1:
+                    edges += 1
+                if i > 0 and grid[i - 1][j] == 1:
+                    edges+=1
+    perimeter = size * 4 - edges * 2           
+                
+    
+    return perimeter
+
+
+
 
     
