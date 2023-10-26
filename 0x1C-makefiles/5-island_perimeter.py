e7966ed62ac75e9b6fcda1b429b70e48fa056341
bm-197 <bisratmaru111@gmail.com>
2023-10-25 22:21:33 -0700

    task 100

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Technical interview preparation"""
+
+def island_perimeter(grid):
+    """Calculate the perimeter of the island
+
+    Args:
+        grid (2D list): a list of list of integers
+
+    Returns:
+        the perimeter
+    """
+
+    
